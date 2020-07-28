e19115baa02e2ff6373657d36bbaeb629951a6a8
Mario Varas <elasgard@hotmail.es>
2020-07-27 19:58:10 -0500

    (feat): [#1] complexify glitches
    - Abstract glitches
    - Random slice height
    - Number of slices
    - etc.

--- modules/glitches.py
+++ modules/glitches.py
@@ -1,26 +1,41 @@
-from typing import List
-import cv2
+from typing import List, Union
 import random
+import cv2
 
-from .vaporize import get_face_classifier
 from typ import Image as ImageType
+from modules.vaporize import get_face_classifier
 
 
 def glitch(
     img: ImageType,
     area: List[int] = None,
-    face: bool = False
+    face: bool = False,
+    n_slices: int = 20
 ) -> ImageType:
     if face:
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         face_cascade = get_face_classifier()
         faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-        for face in faces:
-            face = [int(element) for element in face]
-            draw_glitch(img, *face)
+        for _face in faces:
+            _face = [int(element) for element in _face]
+            draw_glitch(img, *_face, n_slices)
     else:
-        draw_glitch(img, *area)
+        draw_glitch(img, *area, n_slices)
 
+    return img
+
+
+def abstract_glitch(
+    img: ImageType,
+    area: List[int] = None,
+    n_slices: int = 20
+) -> ImageType:
+    draw_glitch(
+        img,
+        *area,
+        n_slices=n_slices,
+        abstract=True
+    )
     return img
 
 
@@ -29,15 +44,48 @@ def draw_glitch(
     area_x: int,
     area_y: int,
     area_w: int,
-    area_h: int
+    area_h: int,
+    n_slices: int,
+    abstract: bool = False
 ) -> ImageType:
-    n_glitches = 20
-    glitch_height = round(area_h / n_glitches)
-    for itr in range(0, n_glitches):
-        glitch_start_y = area_y + (itr * glitch_height)
-        glitch_end_y = area_y + (itr * glitch_height + glitch_height)
-        dist = random.randint(15, area_w // 2)
-        img[glitch_start_y:glitch_end_y, (area_x + dist):area_x + area_w + dist] = \
+    _, img_width, _ = img.shape
+    slice_heights = get_slice_height(area_h, n_slices, equals=True)
+    prev_slice_height = 0
+
+    for itr in range(0, n_slices):
+        slice_height = slice_heights[itr]
+        if not abstract:
+            inc = itr * slice_height
+        else:
+            inc = prev_slice_height
+        glitch_start_y = area_y + inc
+        glitch_end_y = area_y + (sum(slice_heights[:itr]) + slice_height)
+        dist = random.randint(15, 30)
+
+        glitch_w = area_x + dist
+        if glitch_w + area_w > img_width:
+            glitch_w -= (glitch_w + area_w - img_width)
+        img[glitch_start_y:glitch_end_y, glitch_w:glitch_w + area_w] = \
             img[glitch_start_y:glitch_end_y, area_x:area_x + area_w]
 
+        prev_slice_height = slice_height
+
     return img
+
+
+def get_slice_height(
+    area_h: int,
+    n_slices: int = None,
+    equals: bool = True
+) -> Union[int, List[int]]:
+    eq_height = round(area_h / n_slices)
+    if equals:
+        slice_height = [eq_height for _ in range(n_slices)]
+    else:
+        slice_height = []
+        heights = [area_h // (n_slices - 2), area_h // (n_slices + 2)]
+        for _ in range(n_slices):
+            index = random.randint(0, 1)
+            slice_height.append(int(heights[index]))
+
+    return slice_height
