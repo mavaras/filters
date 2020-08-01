71a0d2729563c7c49079fac2192c4e0f993f1360
Mario Varas <elasgard@hotmail.es>
2020-07-31 23:17:00 -0500

    clarify ugly main code :/

--- main.py
+++ main.py
@@ -21,6 +21,8 @@ ARGS = PARSER.parse_args()
 IMG_PATH = ARGS.image or 'imgs/me.jpeg'
 
 def main():
+    # this function for now is only for testing purposes
+
     img = cv2.imread(IMG_PATH)
     # grid = randomize_strokes_order(img)
     h, w, _ = img.shape
@@ -28,12 +30,12 @@ def main():
     area = {
         'x': 0,
         'y': 0,
-        'width': w - 10,
-        'height': h,
+        'width': w - 110,
+        'height': h - 100,
     }
     translation_x = {
         'low_lim': 20,
-        'up_lim': 45
+        'up_lim': 145
     }
     '''img_ress = glitches.offset_rect_colorized(
         img,
@@ -41,18 +43,19 @@ def main():
         channel=2,
         randomize=True
     )'''
-    '''
+    
     img_res = glitches.glitch(
         img,
         translation_x,
         area.values(),
         n_slices=20
-    )'''
+    )
+    '''
     img_res = glitches.cycle_glitch(
         img,
         translation_x,
         *area.values()
-    )
+    )'''
     if ARGS.write_output:
         cv2.imwrite(f'outimgs/out_{uuid.uuid4()}.jpg', img_res)
     cv2.imshow('img_res', img_res)
